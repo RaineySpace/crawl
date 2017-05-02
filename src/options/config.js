19e7bb4e946ec251e4d717360538c21f24209329
Rainey <515385036@qq.com>
2017-05-02 09:39:42 +0800

    update icon

---
+++ src/options/config.js
@@ -0,0 +1,10 @@
+export default {
+    bear: {
+        name: 'bear',
+        logo: './bear-logo.png',
+        default: 'bear://x-callback-url/create?type=html&title=$title$&text=$content$&url=$url$', // &tag=%E9%98%85%E8%AF%BB
+        option: {
+            tag: '%E9%98%85%E8%AF%BB'
+        }
+    }
+};
